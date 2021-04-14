09c0a1395e432885d72b583d34393a45b06cf208
Eabdalhaleem <>
2021-04-14 17:36:57 +0000

    creat jv file

---
+++ new.js
@@ -0,0 +1,24 @@
+//prompt ("please enter your name!")
+
+var username = prompt("what is your name ")
+console.log(username);
+alert(" welcome " + username);
+var petType = prompt(" please enter your pet type (Dog,Cat)?")
+if (petType === "Dog") {
+  alert("we would like to give you our service");
+} else if (petType === "Cat") {
+  alert("we would like to give you our service");
+}
+else {
+  alert("Sorry we can not give your pet a service ")
+
+}
+
+alert("We are here to help you ")
+var petType = prompt(" Do you need any help (yes,no)?")
+if (petType === "yes") {
+  alert("please connact us in this number 0124588 ");
+} else if (petType === "no") {
+  alert("we will happy to help you any time ");
+}
+  alert(" Thank you ");
