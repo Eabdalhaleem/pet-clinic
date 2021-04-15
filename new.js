ce8ef197cf78cc6e34e6f562e67319dfa913ecd7
Eabdalhaleem <>
2021-04-15 14:00:10 +0000

    new qus

---
+++ new.js
@@ -0,0 +1,42 @@
+//prompt ("please enter your name!")
+
+var username = prompt("what is your name ")
+console.log(username);
+alert(" welcome " + username);
+
+var petType = prompt(" please enter your pet type (Dog,Cat)?")
+while (petType !== "Dog" && petType !== "Cat") {
+  petType = prompt("please enter your pet type (Dog,Cat)?")
+}
+
+if (petType === "Dog"){
+var petNumber = Number (prompt("how many Dog do you have?"))
+for (var i = 1; i <= petNumber; i++) {
+  document.write("<img class='pets' src='https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2020%2F10%2F13%2Fcorgi-dog-POPDOGNAME1020.jpg'>")
+}}
+if (petType === "Cat"){
+var petNumber = prompt("how many Cat do you have?")
+for (var i = 1; i <= petNumber; i++) {
+  document.write("<img class='pets' src='https://timesofindia.indiatimes.com/photo/67586673.cms'>")
+}}
+console.log("we will call you ")
+
+// if (petType === "Dog") {
+//   alert("we would like to give you our service");
+// } else if (petType === "Cat") {
+//   alert("we would like to give you our service");
+// }
+// else {
+//   alert("Sorry we can not give your pet a service ")
+
+// }
+
+// alert("We are here to help you ")
+// var petType = prompt(" Do you need any help (yes,no)?")
+// if (petType === "yes") {
+//   alert("please connact us in this number 0124588 ");
+// } else if (petType === "no") {
+//   alert("we will happy to help you any time ");
+// }
+// alert(" Thank you ");
+
